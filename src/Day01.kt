64370798a6dcf73cb08f98e30ad912d9e58d3a9e
Kıraç Acar Apaydın <kirac.apaydin@d-teknoloji.com.tr>
2021-12-17 23:17:16 +0300

    Add solutions for Day01 puzzles

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,13 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<Int>): Int = input.windowed(2).count { it[0] < it[1] }
+
+    fun part2(input: List<Int>): Int {
+        val sums = input.windowed(3) { window -> window.sum() }
+        return sums.windowed(2).count { it[0] < it[1] }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
+    val input = readInput("Day01").map { it.toInt() }
     println(part1(input))
     println(part2(input))
 }
